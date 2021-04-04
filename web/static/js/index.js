85daccb3a1920b743cc4301d200b831df728c73e
Byron Torres <btorres3@hawk.iit.edu>
2021-04-04 05:20:29 -0500

    milestone

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -1,7 +1,57 @@
-const data = { "33": 8, "50": 45, "45": 15, "24": 97, "14": 88, "59": 9, "58": 26, "35": 31, "82": 14, "63": 67, "31": 65, "36": 15, "30": 37, "91": 20, "84": 61, "96": 82, "17": 79, "43": 9, "2": 34, "52": 68, "77": 6, "57": 70, "92": 52 }
+// runs when user clicks button
+AFRAME.registerComponent('start', {
 
+    init: function () {
+        // block pointer events until buttonEnter is clicked
+        let uiDiv = document.getElementById("uiDiv");
+        uiDiv.style["pointer-events"] = "auto";
 
-window.addEventListener('load', event => {
+        // set up background blocker
+        uiDiv.style["background-color"] = "rgba(0, 0, 0, 0.75)";
+
+        // indicate clickable with hand cursor (desktop)
+        let buttonEnter = document.getElementById("buttonEnter");
+        buttonEnter.style.cursor = "pointer";
+
+        // fade-in functionality
+        let fadeIn = function () {
+            // allow point events again
+            uiDiv.style["pointer-events"] = "none";
+
+            // remove startButton
+            buttonEnter.parentNode.remove(buttonEnter);
+
+            // fade out uiDiv background
+            uiDiv.style["background-color"] = "rgba(0, 0, 0, 0.0)";
+            uiDiv.style["transition"] = "background-color 1000ms linear";
+
+            $.ajax({
+                type: "GET",
+                url: "/key_val",
+                success: function (data) {
+                    var myObj = data;
+                    displayData(myObj);
+                }
+            });
+
+            $.ajax({
+                type: "GET",
+                url: "/plane_data",
+                success: function (data) {
+                    var myObj = data;
+                    console.log(myObj);
+                }
+            });
+
+        }
+
+        // activate for desktop/touchscreen
+        buttonEnter.addEventListener('touchstart', fadeIn);
+        buttonEnter.addEventListener('mousedown', fadeIn);
+    }
+});
+
+const displayData = (data) => {
     const ascene = document.getElementById("camera");
     for (var key in data) {
         if (data.hasOwnProperty(key)) {
@@ -13,4 +63,4 @@ window.addEventListener('load', event => {
             ascene.appendChild(node);
         }
     }
-});
+};
