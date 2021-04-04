792a4c9ebaf8568609594440516f653a281031d1
adityadasgupta <aditya.dasgupta1999@gmail.com>
2021-04-04 00:18:12 -0500

    converted to flask app

--- ml.py
+++ ml.py
@@ -1,32 +1,41 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
-import json
+from flask import jsonify
 
 
-   
+# X--> features y--> values randomly generated
+xx = np.empty([25,2])
+yy = np.empty(25)
 
-X = np.array(np.random.randint([0,1],[100,2], size=(25,2)))
+def datagen():
+    X = np.array(np.random.randint([0, 1], [100, 2], size=(25, 2)))
 
-y = np.array(np.random.randint(100, size = 25))
+    y = np.array(np.random.randint(100, size=25))
 
-#y = np.dot(X, np.array([1, 2])) + 3
+    return X, y
 
-keyVal = {}
-for i,n in enumerate(X):
-    keyVal[int(n[0])] = int(y[i])
 
-#print(keyVal)
-#json_obj = json.dumps(keyVal)
+# y = np.dot(X, np.array([1, 2])) + 3
 
-with open('keyVal.json', 'w') as fp:
-    json.dump(keyVal, fp)
- 
-model = LinearRegression().fit(X,y)
+def keyvalgen():
+    xx, yy = datagen()
+    keyVal = {}
+    for i, n in enumerate(xx):
+        keyVal[int(n[0])] = int(yy[i])
+    return jsonify(keyVal)
 
-planeData = {}
 
-planeData['coef'] = float(model.coef_[0])
-planeData['intercept'] = model.intercept_ 
+# print(keyVal)
+# json_obj = json.dumps(keyVal)
 
-with open('lineq.json', 'w') as fp:
-    json.dump(planeData, fp)
+# with open('keyVal.json', 'w') as fp:
+#    json.dump(keyVal, fp)
+def planedatagen():
+    model = LinearRegression().fit(xx, yy)
+
+    planeData = {}
+
+    planeData['coef'] = float(model.coef_[0])
+    planeData['intercept'] = model.intercept_
+
+    return jsonify(planeData)
