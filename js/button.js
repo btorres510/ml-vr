945b650ee92948be5a900b0e5988a57d675e4c7c
Byron Torres <btorres3@hawk.iit.edu>
2021-04-03 19:50:31 -0500

    button function

---
+++ js/button.js
@@ -0,0 +1,37 @@
+// runs when user clicks button
+AFRAME.registerComponent('start', {
+
+    init: function () {
+        // block pointer events until buttonEnter is clicked
+        let uiDiv = document.getElementById("uiDiv");
+        uiDiv.style["pointer-events"] = "auto";
+
+        // set up background blocker
+        uiDiv.style["background-color"] = "rgba(0, 0, 0, 0.75)";
+
+        // indicate clickable with hand cursor (desktop)
+        let buttonEnter = document.getElementById("buttonEnter");
+        buttonEnter.style.cursor = "pointer";
+
+        // fade-in functionality
+        let fadeIn = function () {
+            // allow point events again
+            uiDiv.style["pointer-events"] = "none";
+
+            // sounds can only be triggered after a mouse interaction
+            // let soundPlayer = document.querySelector("#ambientSound");
+            // soundPlayer.components.sound.playSound();
+
+            // remove startButton
+            buttonEnter.parentNode.remove(buttonEnter);
+
+            // fade out uiDiv background
+            uiDiv.style["background-color"] = "rgba(0, 0, 0, 0.0)";
+            uiDiv.style["transition"] = "background-color 1000ms linear";
+        }
+
+        // activate for desktop/touchscreen
+        buttonEnter.addEventListener('touchstart', fadeIn);
+        buttonEnter.addEventListener('mousedown', fadeIn);
+    }
+});
